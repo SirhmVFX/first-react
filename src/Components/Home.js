1eba9fb7f3a45a0b9e5aae229e2bb9517f78c7c4
sirhmvfx <sirhmvfx@gmail.com>
2023-06-25 03:01:56 +0100

    okay

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import { React, useState } from "react";
+import Navbar from "./Navbar,js";
+
+function Home() {
+  return (
+    <div className="home-comntainer">
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
